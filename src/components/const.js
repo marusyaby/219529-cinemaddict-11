612cead5ef17b7a69783fac53ab6fab5c05cd024
marusyaby <mariakhrol@gmail.com>
2020-04-21 13:08:04 +0300

    feat: add profile titles

--- src/components/const.js
+++ src/components/const.js
@@ -1,5 +1,23 @@
 const DESCRIPTION_LENGTH_MAX = 140;
 
+const USER_TITLES = [
+  {
+    NAME: `Novice`,
+    FILMS_MIN: 1,
+    FILMS_MAX: 10
+  },
+  {
+    NAME: `Fan`,
+    FILMS_MIN: 11,
+    FILMS_MAX: 20
+  },
+  {
+    NAME: `Movie Buff`,
+    FILMS_MIN: 21,
+    FILMS_MAX: Number.MAX_VALUE
+  }
+];
+
 const Emoji = {
   ANGRY: `angry.png`,
   PUKE: `puke.png`,
@@ -9,5 +27,6 @@ const Emoji = {
 
 export {
   DESCRIPTION_LENGTH_MAX,
+  USER_TITLES,
   Emoji,
 };
