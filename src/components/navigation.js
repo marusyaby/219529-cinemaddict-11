c7dc8ba44dc921ce945618da30c244394e06d240
marusyaby <mariakhrol@gmail.com>
2020-04-21 16:58:20 +0300

    feat: add filters count

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,11 +1,50 @@
-const createNavigationTemplate = () => {
+const filterFilms = (filmsArray) => {
+  return ([
+    {
+      name: `All movies`,
+      results: filmsArray,
+    },
+    {
+      name: `Watchlist`,
+      results: filmsArray.filter((film) =>
+        film.isInList),
+    },
+    {
+      name: `History`,
+      results: filmsArray.filter((film) =>
+        film.isWatched),
+    },
+    {
+      name: `Favorites`,
+      results: filmsArray.filter((film) =>
+        film.isFavourite),
+    }
+  ]);
+};
+
+const createFiltersMarkup = (filmsArray) => {
+  const filters = filterFilms(filmsArray);
+
+  return filters.map((filter, index) => {
+    const {name, results} = filter;
+
+    const activeClass = index === 0 ? `main-navigation__item--active` : ``;
+    const count = index === 0 ? `` : `<span class="main-navigation__item-count">${results.length}</span>`;
+
+    return (
+      `<a href="#${name}" class="main-navigation__item ${activeClass}">
+        ${name}
+        ${count}
+       </a>`
+    );
+  }).join(`\n`);
+};
+
+const createNavigationTemplate = (filmsArray) => {
   return (
     `<nav class="main-navigation">
       <div class="main-navigation__items">
-        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+        ${createFiltersMarkup(filmsArray)}
       </div>
       <a href="#stats" class="main-navigation__additional">Stats</a>
     </nav>`
